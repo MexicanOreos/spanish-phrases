e6e9d28590f304267cc765821cb17bb612e1db1e
Victor Sanchez Gutierrez <sanchez.m.victor97@gmail.com>
2018-07-16 15:06:29 -0700

    Changed Map to arrayList to utilize arrayAdapter with listView (Testing GridView)

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,9 +3,13 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -17,26 +21,36 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
+        ArrayList<String> numbersList = new ArrayList<>();
+
         LinkedHashMap<String, String> numbersMap = new LinkedHashMap<>();
 
-        numbersMap.put("One", "Uno");
-        numbersMap.put("Two", "Dos");
-        numbersMap.put("Three", "Tres");
-        numbersMap.put("Four", "Quatro");
-        numbersMap.put("Five", "Cinco");
-        numbersMap.put("Six", "Sies");
-        numbersMap.put("Seven", "Siete");
-        numbersMap.put("Eight", "Ocho");
-        numbersMap.put("Nine", "Nueve");
-        numbersMap.put("Ten", "Diez");
+        numbersList.add("One");
+        numbersList.add("Two");
+        numbersList.add("Three");
+        numbersList.add("Four");
+        numbersList.add("Five");
+        numbersList.add("Six");
+        numbersList.add("Seven");
+        numbersList.add("Eight");
+        numbersList.add("Nine");
+        numbersList.add("Ten");
 
-        //We store the LinearLayout to be able to reference it and add child text views
-        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
-        for(Map.Entry numbers : numbersMap.entrySet()) {
-            TextView wordView = new TextView(this); //Create a new wordview
-            wordView.setText((String) numbers.getKey()); //set it's text
-            rootView.addView(wordView); // add it to layout
-        }
+//        //We store the LinearLayout to be able to reference it and add child text views
+//        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+//        for(Map.Entry numbers : numbersMap.entrySet()) {
+//            TextView wordView = new TextView(this); //Create a new wordview
+//            wordView.setText((String) numbers.getKey()); //set it's text
+//            rootView.addView(wordView); // add it to layout
+//        }
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, numbersList);
+//        ListView listView = (ListView) findViewById(R.id.list);
+//        listView.setAdapter(arrayAdapter);
+        GridView gridView = (GridView) findViewById(R.id.list);
+        gridView.setAdapter(arrayAdapter);
+
+
 
 
 
